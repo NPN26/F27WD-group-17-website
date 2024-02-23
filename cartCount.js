dc7d497efebe183db38ac38a9459de2753b298c3
NPN26 <133509488+NPN26@users.noreply.github.com>
2024-02-23 19:49:14 +0400

    Added account page

--- cartCount.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-    var existingData = JSON.parse(localStorage.getItem("userData")) || [];
-    var pcount = existingData.length;
-
-    var cartBadge = document.getElementById('cartCount');
-    cartBadge.innerHTML = pcount;
-}
